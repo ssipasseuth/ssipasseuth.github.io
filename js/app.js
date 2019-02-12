53b50afd888d47ca03efd2f2867a79ddf738c009
Sam <sam@taotesting.com>
2019-02-12 17:54:02 +0100

    disable swipe

--- js/app.js
+++ js/app.js
@@ -184,6 +184,7 @@ $(function () {
     });
 
     swipedetect(swipeArea, function(swipedir){
+        return;
         switch(swipedir){
             case 'right':
                 carousel.previous();
